30beeb595ac7594bae85e57cb1b949c6f4355a6c
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-07-21 20:44:57 -0700

    added the suitable comments

--- src/CeilInBST.java
+++ src/CeilInBST.java
@@ -1,5 +1,6 @@
 public class CeilInBST {
 
+    //node
     class Node{
         int val;
         Node left;
@@ -10,13 +11,17 @@ public class CeilInBST {
             this.right = right;
         }
     }
+    //ceil is a number in the tree which is greater than or equals to the given target/ key
 
     public static int ceilInBST(Node root, int key){
         if(root ==null) return -1;
 
         Node current = root;
         int ans =0;
+        //until it reaches the end
         while(current != null){
+            //basically, it will check for the the other smaller element if its greater,
+            // as left side of bst stores smaller values
             if(current.val>= key ){
                 ans = current.val;
                 current = current.left;
